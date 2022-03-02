168ee07c36ed155aa5fa0d31369cce71ebf420f9
Jonathan Cheng <jcheng461@gmail.com>
2022-03-02 23:11:09 +0900

    make 5 form questions. When user submits, scripts tally up the value of each answer given. Considering the total points, user gets a response saying what Simpsons character they are most like.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $("form#survey").submit(function (event) {
+    event.preventDefault();
+    const q1 = parseInt($("#question1").val());
+    const q2 = parseInt($("#question2").val());
+    const q3 = parseInt($("#question3").val());
+    const q4 = parseInt($("input:radio[name=question4]:checked").val());
+    const q5 = parseInt($("input:radio[name=question5]:checked").val());
+
+    const total = q1 + q2 + q3 + q4 + q5;
+
+    if (total >= 5 && total <= 10) {
+      $("#character").text("Bart Simpson");
+    } else if (total >= 11 && total <= 15) {
+      $("#character").text("Lisa Simpson");
+    } else if (total >= 16 && total <= 20) {
+      $("#character").text("Maggie Simpson");
+    }
+  });
+});
